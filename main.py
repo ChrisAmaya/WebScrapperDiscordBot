dc58a7ea20d1f8a5f5d30279ffed4185487b1acd
Addi Amaya <caa746@usask.ca>
2022-05-19 16:28:21 -0600

    initial commit with main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Author: Addi Amaya
+# Last Editted: May 19, 2022
+#
+# Grabs the Newsletter from the University of Saskatchewan and College of Engineering 
+# and post in chat periodically and on demand
+
+import discord
+import os
+# import search_runpee
