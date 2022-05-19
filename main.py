bfc9b5b4fd15283d658c1a744b52a8a1d22e154d
Addi Amaya <caa746@usask.ca>
2022-05-19 17:49:14 -0600

    add start up msg and simple respond text to main.py

--- main.py
+++ main.py
@@ -5,5 +5,27 @@
 # and post in chat periodically and on demand
 
 import discord
-import os
-# import search_runpee
+
+# Instatiate discord Client
+client = discord.Client()
+
+# Discord Event to check when the bot is online
+@client.event
+async def on_ready(): 
+    print(f'{client.user} is now online!')
+
+# Discord Event to response to message
+@client.event
+async def on_message(message):
+    
+    # make sure bot doesnt respond to its own message to avoid infinite loop
+    if message.author == client.user:
+        return
+      # lower case message
+    message_content = message.content.lower()  
+    
+    if message.content.startswith(f'$hello'):
+        await message.channel.send('''Fuck you.''')
+    
+# Retrieve Token
+client.run("OTc2OTY4NTgxOTczNjkyNDc2.G9_FY5.b4Rqk3g1NuMs20oJbowRZmerWjrzrDWglBvekA")
