a269c9fcaa936cff1e39fe9078ed0cddbad7e47e
Addi Amaya <caa746@usask.ca>
2022-05-19 21:43:29 -0600

    working sorta search without expaning page

--- UniversityScrapper.py
+++ UniversityScrapper.py
@@ -4,20 +4,18 @@ from bs4 import BeautifulSoup
 class UniversityNews:
     def __init__(self):
         self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'}
-        self.url = 'https://news.usask.ca/'
+        self.url = 'https://news.usask.ca/index.php'
 
     # Create search words and url keywords part
-    def key_words_search_words(self, user_message):
+    def search_words(self, user_message):
         words = user_message.split()[1:]
-        print(f'key_words_search_words: The words are "{words}"')
-        keywords = '-'.join(words)
         search_words = ' '.join(words)
-        return keywords, search_words
+        print(f"Search_Words: {search_words}")
+        return search_words
     
-    def search(self, keywords):
+    def search(self):
         
         # create requests and get the response
-        # response = requests.get(self.url+keywords, headers = self.headers)
         response = requests.get(self.url)
         content = response.content
         
@@ -32,6 +30,7 @@ class UniversityNews:
         send_link = set()
         for link in result_links:
             text = link.text.lower()
+            print(f"Send_link: {text}")
             if search_words in text:
-                send_link.add(str(self.url)+str(link.get('href')))
+                send_link.add("https://news.usask.ca/"+str(link.get('href')))
         return send_link
--- main.py
+++ main.py
@@ -36,13 +36,19 @@ async def on_message(message):
         await message.channel.send('''Fuck you.''')
         
     if f'$search' in message_content:
-        key_words, search_words = university_news.key_words_search_words(message_content)
-        result_links = university_news.search(key_words)
+        
+        search_words = university_news.search_words(message_content)
+        result_links = university_news.search()
         links = university_news.send_link(result_links, search_words)
         
+        counter = 0
         if len(links) > 0:
             for link in links:
-                await message.channel.send(link)
+                if (counter >= 10):
+                    break
+                else:
+                    await message.channel.send(link)
+                    counter += 1
         else:
             await message.channel.send(no_result_message)
     
